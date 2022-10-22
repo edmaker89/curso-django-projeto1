cfa769afe1197a7c21e98621e9d7cd86656f7334
Douglas Silva <edmaker@gmail.com>
2022-10-22 19:37:02 -0300

    factory

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Douglas'
+        'recipes': [make_recipe() for _ in range(10)],  # list comprehension
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Douglas'
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
